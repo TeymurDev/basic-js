38a392b4d19634f88f8d59c3f93c0d7b33de7066
Teymur <you@example.com>
2021-03-04 21:43:58 +0400

    feat: finish task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if(!(Array.isArray(members))) { 
+    return false;
+  }
+
+  const ignoreOtherTypes = members.filter(item => typeof item === 'string');
+  const capitalizedAndTrimmed = ignoreOtherTypes.map(item => item.toUpperCase().trim());
+  const firstLetters = capitalizedAndTrimmed.map(([item])=> item);
+  const sortedAndConcatenated = firstLetters.sort().join('');
+
+  return sortedAndConcatenated;
+
 };
